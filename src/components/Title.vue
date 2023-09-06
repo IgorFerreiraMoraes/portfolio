808f4f412bd9bb0900053e32669429008d11ff95
Igor Ferreira Moraes <igorfm13524@gmail.com>
2023-09-06 15:33:52 -0300

    Adiciona componenete de títulos dinâmicos

---
+++ src/components/Title.vue
@@ -0,0 +1,31 @@
+<script setup>
+	const props = defineProps(['size']);
+
+	const sizes = {
+		large: 'h1',
+		medium: 'h2',
+		small: 'h3',
+	};
+</script>
+
+<template>
+	<component :is="sizes[size]">
+		<slot></slot>
+	</component>
+</template>
+
+<style scoped>
+	h1 {
+		color: var(--color-white);
+		font: var(--title-large);
+		letter-spacing: -0.8px;
+	}
+	h2 {
+		color: var(--color-white);
+		font: var(--title-medium);
+		letter-spacing: -0.24px;
+	}
+	h3 {
+		font: var(--title-small);
+	}
+</style>
