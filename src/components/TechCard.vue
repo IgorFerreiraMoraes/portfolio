caefe9de99e16532ae753c7e1fa69462d0c3a975
Igor Ferreira Moraes <igorfm13524@gmail.com>
2023-09-08 13:33:10 -0300

    Cria `TechCard.vue`

---
+++ src/components/TechCard.vue
@@ -0,0 +1,59 @@
+<script setup></script>
+<template>
+	<div class="tech-card">
+		<h3><slot name="title"></slot></h3>
+		<p class="content">
+			<slot name="content"></slot>
+		</p>
+		<div class="buttons">
+			<button>anterior</button>
+			<button>próximo</button>
+		</div>
+	</div>
+</template>
+<style scoped>
+	.tech-card {
+		background-color: var(--color-white);
+		width: 81.5%;
+		height: 16.8125rem;
+		margin: auto;
+		border-radius: 0.5rem;
+		padding: 3rem 1.5rem;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
+	h3 {
+		color: var(--color-background);
+		font: var(--body-large);
+		letter-spacing: -0.2px;
+		text-align: center;
+		margin-bottom: 1rem;
+	}
+	.tech-card .content {
+		font: var(--body-small);
+		color: var(--color-dark-accent);
+	}
+	.buttons {
+		display: flex;
+		width: 100%;
+		justify-content: space-evenly;
+		margin-top: auto;
+	}
+	.buttons > button {
+		background-color: transparent;
+		font: var(--body-small);
+		color: #00b6da;
+		transition: 0.2s;
+		cursor: pointer;
+	}
+	.buttons > button:hover {
+		color: var(--color-main);
+	}
+</style>
+<style>
+	.tech-card .content > img {
+		width: 1.6875rem;
+		vertical-align: bottom;
+	}
+</style>
