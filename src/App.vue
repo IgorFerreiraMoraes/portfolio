3aa78251284f3b695ddaaea8cefbe69a0523565e
Igor Ferreira Moraes <igorfm13524@gmail.com>
2023-09-09 00:56:02 -0300

    Cria primeira seção do site `SectionHero.vue`

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,10 @@
 <script setup>
-	import ButtonPrimary from './components/ButtonPrimary.vue';
-	import ButtonSecondary from './components/ButtonSecondary.vue';
-	import Title from './components/Title.vue';
+	import SectionHero from './components/SectionHero.vue';
 </script>
 
 <template>
 	<main>
-		<Title size="large">Igor Moraes</Title>
-		<Title size="medium">Front-end & Designer</Title>
-		<Title size="small">Bora criar algo fascinante juntos</Title>
-		<br />
-		<ButtonPrimary link="https://github.com/"> Conheça </ButtonPrimary>
-		<br />
-		<ButtonSecondary
-			link="https://github.com/"
-			image="./src/assets/images/social/social-instagram.svg"
-			>Instagram</ButtonSecondary
-		>
+		<SectionHero></SectionHero>
 	</main>
 </template>
 
---
+++ src/components/SectionHero.vue
@@ -0,0 +1,56 @@
+<script setup>
+	import Title from './Title.vue';
+	import ButtonPrimary from './ButtonPrimary.vue';
+	import ButtonSecondary from './ButtonSecondary.vue';
+
+	const link_whatsapp =
+		'https://wa.me/5511941285976?text=Ol%C3%A1,%20Igor!%20Tenho%20interesse%20em%20seus%20servi%C3%A7os,%20vamos%20conversar%20sobre%20como%20voc%C3%AA%20pode%20me%20ajudar?';
+</script>
+<template>
+	<section id="hero">
+		<span>Vivamus gravida, nunc egestas faucibus porttitor</span>
+		<Title size="large">Igor Moraes</Title>
+		<Title size="medium">Front-end & Designer</Title>
+		<p>
+			Transformando ideias em realidade através de design e
+			desenvolvimento web. Vem subir sua visibilidade online comigo!
+		</p>
+		<img src="../assets/images/raven.svg" alt="" />
+		<ButtonPrimary :link="link_whatsapp" style="margin-bottom: 1.5rem"
+			>Entrar em contato</ButtonPrimary
+		>
+		<ButtonSecondary
+			link="https://www.instagram.com/igor.f.moraes/"
+			image="./src/assets/images/social/social-instagram.svg"
+			>Instagram</ButtonSecondary
+		>
+	</section>
+</template>
+<style scoped>
+	section {
+		position: relative;
+	}
+	p {
+		margin-top: 0.5rem;
+		margin-bottom: 1.5rem;
+		font: var(--body-medium);
+		color: var(--color-text);
+		letter-spacing: -0.2px;
+	}
+	span {
+		margin-bottom: 4rem;
+		font-family: 'Anchrish Runes';
+		font-size: 1.219rem;
+		opacity: 30%;
+		font-weight: 400;
+		display: block;
+		text-align: center;
+	}
+	img {
+		position: absolute;
+		width: min(70%, 15rem);
+		top: 18%;
+		left: 35%;
+		z-index: -1;
+	}
+</style>
